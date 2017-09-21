8ebd8edaae4fc6d92412b20656b7943a4953b001
Philip <philipkarpovich@gmail.com>
2017-09-21 11:10:21 +0300

    initial

--- src/tic-tac-toe.js
+++ src/tic-tac-toe.js
@@ -1,35 +1,148 @@
 class TicTacToe {
-    constructor() {
+  constructor() {
 
+    this.state = {
+      symbol:     'x',
+      isFinished: null,
+      noTurns:    false,
+      winner:     false,
+      draw:       false,
+      symWin:     '',
+      field:      [
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', '']
+      ]
+    }
+  }
+
+  getCurrentPlayerSymbol() {
+    return this.state.symbol;
+  }
+
+  nextTurn(rowIndex, columnIndex) {
+    var sym = this.state.symbol;
+
+    if (this.state.noTurns) {
+      if (this.state.winner) {
+        this.state.isFinished = true;
+        this.isFinished();
+        return
+      } else {
+        this.state.draw = true;
+        this.isDraw();
+        return
+      }
     }
 
-    getCurrentPlayerSymbol() {
-
+    if (this.state.isFinished) {
+      return;
     }
 
-    nextTurn(rowIndex, columnIndex) {
+    var pos = this.state.field[rowIndex][columnIndex];
 
+    if (pos === 'x' || pos === 'o') {
+      return
     }
 
-    isFinished() {
+    if (this.state.symbol === 'x') {
+      this.state.symbol = 'o';
+    } else this.state.symbol = 'x'
 
+    this.state.field[rowIndex][columnIndex] = sym;
+
+    if (this.state.field[0][0] === sym && this.state.field[0][1] === sym && this.state.field[0][2] === sym) {
+      this.state.symWin = sym;
+      this.state.isFinished = true;
+      this.state.winner = true;
+      return
+    } else if (this.state.field[1][0] === sym && this.state.field[1][1] === sym && this.state.field[1][2] === sym) {
+      this.state.symWin = sym;
+      this.state.isFinished = true;
+      this.state.winner = true;
+      return
+    } else if (this.state.field[2][0] === sym && this.state.field[2][1] === sym && this.state.field[2][2] === sym) {
+      this.state.symWin = sym;
+      this.state.isFinished = true;
+      this.state.winner = true;
+      return
     }
 
-    getWinner() {
-
+    if (this.state.field[0][0] === sym && this.state.field[1][0] === sym && this.state.field[2][0] === sym) {
+      this.state.symWin = sym;
+      this.state.isFinished = true;
+      this.state.winner = true;
+      return
+    } else if (this.state.field[0][1] === sym && this.state.field[1][1] === sym && this.state.field[2][1] === sym) {
+      this.state.symWin = sym;
+      this.state.isFinished = true;
+      this.state.winner = true;
+      return
+    } else if (this.state.field[0][2] === sym && this.state.field[1][2] === sym && this.state.field[2][2] === sym) {
+      this.state.symWin = sym;
+      this.state.isFinished = true;
+      this.state.winner = true;
+      return
     }
 
-    noMoreTurns() {
-
+    if (this.state.field[0][0] === sym && this.state.field[1][1] === sym && this.state.field[2][2] === sym) {
+      this.state.symWin = sym;
+      this.state.isFinished = true;
+      this.state.winner = true;
+    } else if (this.state.field[0][2] === sym && this.state.field[1][1] === sym && this.state.field[2][0] === sym) {
+      this.state.symWin = sym;
+      this.state.isFinished = true;
+      this.state.winner = true;
     }
 
-    isDraw() {
 
+
+    this.noMoreTurns();
+  }
+
+  isFinished() {
+    return this.state.isFinished ?
+      this.state.symWin
+      :
+      null
+  }
+
+  getWinner() {
+    return this.state.winner ?
+      this.state.symWin
+      :
+      null
+  }
+
+  noMoreTurns() {
+    for (var i = 0; i < this.state.field.length; i++) {
+      for (var j = 0; j < this.state.field[i].length; j++) {
+        if (this.state.field[i][j] === '') {
+          this.state.noTurns = false;
+          return false;
+        }
+      }
     }
+    this.state.noTurns = true;
+    this.state.isFinished = true;
+    return true;
+  }
 
-    getFieldValue(rowIndex, colIndex) {
-
+  isDraw() {
+    if (this.state.isFinished === null) {
+      this.state.draw = false;
+      return false
+    } else if (this.state.winner === true) {
+      this.state.draw = false;
+      return false
     }
+    return true
+  }
+
+  getFieldValue(rowIndex, colIndex) {
+    var value = this.state.field[rowIndex][colIndex];
+    return value === "" ? null : value
+  }
 }
 
 module.exports = TicTacToe;
